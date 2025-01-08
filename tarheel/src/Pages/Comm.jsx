e6882319e06cea63b7e952e059d81d25193403d7
preetha-sivakumar <preethasivakumar111@gmail.com>
2025-01-08 11:37:21 -0500

    navbar links changes

--- tarheel/src/Pages/Comm.jsx
+++ tarheel/src/Pages/Comm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Contact from '../components/Contact/Contact'
 import Footer from '../components/Footer/Footer'
-import Navbar from '../components/Navbar/Navbar'
+import Navbar from '../components/Navbar1/Navbar'
 
 const Comm = () => {
   return (
--- tarheel/src/Pages/Services_page.jsx
+++ tarheel/src/Pages/Services_page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import Navbar from '../components/Navbar1/Navbar'
 import { Services } from '../components/Services/Services'
 import Footer from '../components/Footer/Footer'
 
